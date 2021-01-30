1b8e1422d1e4034a07b90cb091dc85f5a9938fed
Joey Wang <qfhy2005@gmail.com>
2021-01-30 10:02:10 -0600

    Update format

--- SourceCode/kill/server/src/main/java/com/debug/kill/server/controller/KillController.java
+++ SourceCode/kill/server/src/main/java/com/debug/kill/server/controller/KillController.java
@@ -24,6 +24,7 @@ import javax.servlet.http.HttpSession;
 
 /**
  * 秒杀controller
+ *
  * @Author:debug (SteadyJack)
  * @Date: 2019/6/17 22:14
  **/
@@ -41,44 +42,37 @@ public class KillController {
     private ItemKillSuccessMapper itemKillSuccessMapper;
 
 
-
     /***
      * 商品秒杀核心业务逻辑
      * @param dto
      * @param result
      * @return
      */
-    @RequestMapping(value = prefix+"/execute",method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(value = prefix + "/execute", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
-    public BaseResponse execute(@RequestBody @Validated KillDto dto, BindingResult result, HttpSession session){
-        if (result.hasErrors() || dto.getKillId()<=0){
+    public BaseResponse execute(@RequestBody @Validated KillDto dto, BindingResult result, HttpSession session) {
+        if (result.hasErrors() || dto.getKillId() <= 0) {
             return new BaseResponse(StatusCode.InvalidParams);
         }
-        Object uId=session.getAttribute("uid");
-        if (uId==null){
+        Object uId = session.getAttribute("uid");
+        if (uId == null) {
             return new BaseResponse(StatusCode.UserNotLogin);
         }
         //Integer userId=dto.getUserId();
-        Integer userId= (Integer)uId ;
+        Integer userId = (Integer) uId;
 
-        BaseResponse response=new BaseResponse(StatusCode.Success);
+        BaseResponse response = new BaseResponse(StatusCode.Success);
         try {
             //Boolean res=killService.killItem(dto.getKillId(),userId);
-            Boolean res=killService.killItem(dto.getKillId(),userId);
-            if (!res){
-                return new BaseResponse(StatusCode.Fail.getCode(),"哈哈~商品已抢购完毕或者不在抢购时间段哦!");
+            Boolean res = killService.killItem(dto.getKillId(), userId);
+            if (!res) {
+                return new BaseResponse(StatusCode.Fail.getCode(), "Sold out or expired!");
             }
-        }catch (Exception e){
-            response=new BaseResponse(StatusCode.Fail.getCode(),e.getMessage());
+        } catch (Exception e) {
+            response = new BaseResponse(StatusCode.Fail.getCode(), e.getMessage());
         }
         return response;
     }
-
-
-
-
-
-
 
 
     /***
@@ -87,13 +81,13 @@ public class KillController {
      * @param result
      * @return
      */
-    @RequestMapping(value = prefix+"/execute/lock",method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(value = prefix + "/execute/lock", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
-    public BaseResponse executeLock(@RequestBody @Validated KillDto dto, BindingResult result){
-        if (result.hasErrors() || dto.getKillId()<=0){
+    public BaseResponse executeLock(@RequestBody @Validated KillDto dto, BindingResult result) {
+        if (result.hasErrors() || dto.getKillId() <= 0) {
             return new BaseResponse(StatusCode.InvalidParams);
         }
-        BaseResponse response=new BaseResponse(StatusCode.Success);
+        BaseResponse response = new BaseResponse(StatusCode.Success);
         try {
             //不加分布式锁的前提
             /*Boolean res=killService.killItemV2(dto.getKillId(),dto.getUserId());
@@ -114,54 +108,48 @@ public class KillController {
             }*/
 
             //基于ZooKeeper的分布式锁进行控制
-            Boolean res=killService.killItemV5(dto.getKillId(),dto.getUserId());
-            if (!res){
-                return new BaseResponse(StatusCode.Fail.getCode(),"基于ZooKeeper的分布式锁进行控制-哈哈~商品已抢购完毕或者不在抢购时间段哦!");
+            Boolean res = killService.killItemV5(dto.getKillId(), dto.getUserId());
+            if (!res) {
+                return new BaseResponse(StatusCode.Fail.getCode(), "基于ZooKeeper的分布式锁进行控制-哈哈~商品已抢购完毕或者不在抢购时间段哦!");
             }
 
-        }catch (Exception e){
-            response=new BaseResponse(StatusCode.Fail.getCode(),e.getMessage());
+        } catch (Exception e) {
+            response = new BaseResponse(StatusCode.Fail.getCode(), e.getMessage());
         }
         return response;
     }
-
-
-
-
-
-
-
 
 
     //http://localhost:8083/kill/kill/record/detail/343147116421722112
 
     /**
      * 查看订单详情
+     *
      * @return
      */
-    @RequestMapping(value = prefix+"/record/detail/{orderNo}",method = RequestMethod.GET)
-    public String killRecordDetail(@PathVariable String orderNo, ModelMap modelMap){
-        if (StringUtils.isBlank(orderNo)){
+    @RequestMapping(value = prefix + "/record/detail/{orderNo}", method = RequestMethod.GET)
+    public String killRecordDetail(@PathVariable String orderNo, ModelMap modelMap) {
+        if (StringUtils.isBlank(orderNo)) {
             return "error";
         }
-        KillSuccessUserInfo info=itemKillSuccessMapper.selectByCode(orderNo);
-        if (info==null){
+        KillSuccessUserInfo info = itemKillSuccessMapper.selectByCode(orderNo);
+        if (info == null) {
             return "error";
         }
-        modelMap.put("info",info);
+        modelMap.put("info", info);
         return "killRecord";
     }
 
 
     //抢购成功跳转页面
-    @RequestMapping(value = prefix+"/execute/success",method = RequestMethod.GET)
-    public String executeSuccess(){
+    @RequestMapping(value = prefix + "/execute/success", method = RequestMethod.GET)
+    public String executeSuccess() {
         return "executeSuccess";
     }
 
     //抢购失败跳转页面
-    @RequestMapping(value = prefix+"/execute/fail",method = RequestMethod.GET)
-    public String executeFail(){
+    @RequestMapping(value = prefix + "/execute/fail", method = RequestMethod.GET)
+    public String executeFail() {
         return "executeFail";
     }
 
